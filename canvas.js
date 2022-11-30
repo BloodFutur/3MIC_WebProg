16c23c020531d58f37e18ed31435c741b3614561
Olougouna Axel <olougoun@insa-20707>
2022-11-30 15:16:00 +0100

    dernier commit de la journée

--- canvas.js
+++ canvas.js
@@ -4,9 +4,19 @@ canvas = document.getElementById('canvas');
 function DrawGameCub(x, y){
     ctx = canvas.getContext('2d');
     const a = 10;
-    ctx.fillStyle='brown';
+    ctx.fillStyle='#DEB887';
     ctx.fillRect(x, y, a, a);
     ctx.save();
+}
+//fonction dessiner cube de bois avec img
+function imgDrawGameCub(x,y){
+    ctx = canvas.getContext('2d');
+    const dim = 100;
+    let cubebois = new Image();
+    cubebois.src = //lien de l'image;
+    cubebois.addEventListener('load',function(){
+    ctx.drawImage(cubebois,x,y,dim,dim);
+    },false);
 }
 
 //fonction pour dessiner des cercles du jeu
@@ -19,6 +29,16 @@ function Drawcircle(x, y){
     ctx.fillStyle="grey";
     ctx.stroke();
     ctx.save();
+}
+//function pour dessiner des cercles  à partir de l'image
+function imgDrawcircle(x,y){
+    ctx = canvas.getContext('2d');
+    const dim = 100;
+    let circle = new Image();
+    circle.src = //lien de l'image;
+    circle.addEventListener('load',function(){
+    ctx.drawImage(circle,x,y,dim,dim);
+    },false);
 }
 
 //fonction pour dessiner un cube du labyrinthe
@@ -44,9 +64,34 @@ function Mario(x, y){
     },false);
 }
 
+//function généric qui dessine une image
+function DrawImg(That){
+    ctx = canvas.getContext('2d');
+    const dim = 20;
+    let img= new Image();
+    img.src = //lien de l'image;
+    img.addEventListener('load',function(){
+    ctx.drawImage(img,x,y,dim,dim);
+    },false);
+    ctx.save();
+}
+
 //function qui met à jour le canvas
-function MajJeu(){
-    
+function MajJeu(Mat){
+    while(true){
+        ctx = canvas.getContext('2d');
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        let lignes= 0; // à définir plus tard
+        let colones=0;// à définir plus tard
+        for (let i=0; i< lignes; i++){
+            for(let j=0; j< colones; j++){
+                let obj = Mat[i][j];
+                DrawImg(obj);
+
+            }
+        }
+
+    }
 }
 
 
