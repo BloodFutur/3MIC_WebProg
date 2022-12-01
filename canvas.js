d4b118992db327b62ebb4561a6abf307edfe640e
nbillard <nathanbillard12@gmail.com>
2022-12-01 06:23:57 +0100

    merged script.js and canvas.js and separated them into modules

--- canvas.js
+++ canvas.js
@@ -1,27 +1,25 @@
-canvas = document.getElementById('canvas');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
 
 //fonction pour dessiner un cube de bois du jeu
-function DrawGameCub(x, y){
-    ctx = canvas.getContext('2d');
+function DrawGameCub(ctx, x, y){
     const a = 10;
     ctx.fillStyle='#DEB887';
     ctx.fillRect(x, y, a, a);
     ctx.save();
 }
 //fonction dessiner cube de bois avec img
-function imgDrawGameCub(x,y){
-    ctx = canvas.getContext('2d');
+function imgDrawGameCub(ctx, x,y){
     const dim = 100;
     let cubebois = new Image();
     cubebois.src = //lien de l'image;
     cubebois.addEventListener('load',function(){
-    ctx.drawImage(cubebois,x,y,dim,dim);
+        ctx.drawImage(cubebois,x,y,dim,dim);
     },false);
 }
 
 //fonction pour dessiner des cercles du jeu
-function Drawcircle(x, y){
-    ctx = canvas.getContext('2d');
+function Drawcircle(ctx, x, y){
     //let context=canvas.getContext("2d");
     ctx.beginPath();
     ctx.lineWidth="2"
@@ -31,19 +29,17 @@ function Drawcircle(x, y){
     ctx.save();
 }
 //function pour dessiner des cercles  à partir de l'image
-function imgDrawcircle(x,y){
-    ctx = canvas.getContext('2d');
+function imgDrawcircle(ctx, x,y){
     const dim = 100;
     let circle = new Image();
     circle.src = //lien de l'image;
     circle.addEventListener('load',function(){
-    ctx.drawImage(circle,x,y,dim,dim);
+        ctx.drawImage(circle,x,y,dim,dim);
     },false);
 }
 
 //fonction pour dessiner un cube du labyrinthe
-function DrawLabCub(){
-    ctx = canvas.getContext('2d');
+function DrawLabCub(ctx){
     const a = 10;
     ctx.fillStyle="black";
     ctx.fillRect(x, y, a, a);
@@ -51,8 +47,7 @@ function DrawLabCub(){
 }
 
 //inserer l image de Mario
-function Mario(x, y){
-    ctx = canvas.getContext('2d');
+function Mario(ctx, x, y){
     const dim = 100;
     //let image=document.getElementByid=("id image en html");
     let Marioimg = new Image();
@@ -60,26 +55,24 @@ function Mario(x, y){
     
     //ctx.drawImage(image,x,y,dim,dim);
     Marioimg.addEventListener('load',function(){
-    ctx.drawImage(Marioimg,x,y,dim,dim);
+        ctx.drawImage(Marioimg,x,y,dim,dim);
     },false);
 }
 
 //function généric qui dessine une image
-function DrawImg(That){
-    ctx = canvas.getContext('2d');
+function DrawImg(ctx, tile){
     const dim = 20;
     let img= new Image();
-    img.src = //lien de l'image;
+    img.src = tile.image;//lien de l'image;
     img.addEventListener('load',function(){
-    ctx.drawImage(img,x,y,dim,dim);
+        ctx.drawImage(img,tile.x,tile.y,dim,dim);
     },false);
     ctx.save();
 }
 
 //function qui met à jour le canvas
-function MajJeu(Mat){
+function MajJeu(ctx, Mat){
     while(true){
-        ctx = canvas.getContext('2d');
         ctx.clearRect(0,0, canvas.width, canvas.height);
         let lignes= 0; // à définir plus tard
         let colones=0;// à définir plus tard
@@ -95,5 +88,5 @@ function MajJeu(Mat){
 }
 
 
-DrawGameCub(100,100);
-Mario(200, 100);
+DrawGameCub(ctx, 100,100);
+Mario(ctx, 200, 100);
