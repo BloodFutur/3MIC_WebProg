7eb2e47993b741f4f58eeece72e32aae6dc0226d
nbillard <nathanbillard12@gmail.com>
2022-12-06 05:42:08 +0100

    le bouton évasif

--- modules/timer.mjs
+++ modules/timer.mjs
@@ -2,14 +2,43 @@ export class Timer {
   constructor(time, expireFunction) {
     self.time = time;
     self.timerElement = document.getElementById('timer');
+    self.pauseButton1 = document.getElementById('pause-1');
+    self.pauseButton2 = document.getElementById('pause-2');
+    self.pauseButton2.style.visibility = "hidden";
+    self.pauseButton1.addEventListener("mouseenter", () => {
+      self.pauseButton2.style.visibility = "visible";
+      self.pauseButton1.style.visibility = "hidden";
+    });
+    self.pauseButton1.addEventListener("focus", () => {
+      self.pauseButton2.style.visibility = "visible";
+      self.pauseButton1.style.visibility = "hidden";
+    });
+    self.pauseButton2.addEventListener("mouseenter", () => {
+      self.pauseButton1.style.visibility = "visible";
+      self.pauseButton2.style.visibility = "hidden";
+    });
+    self.pauseButton2.addEventListener("focus", () => {
+      self.pauseButton1.style.visibility = "visible";
+      self.pauseButton2.style.visibility = "hidden";
+    });
+    self.pauseButton2.addEventListener("click", () => {
+      alert("All this effort for nothing");
+    });
+    self.pauseButton1.addEventListener("cilck", () => {
+      alert("All this effort for nothing");
+    });
+    self.timeRunning = true;
     self.intervalController = setInterval(() => {
       // self.timerElement.innerHTML = "Time : " + String(self.time).padStart(5, ' ').InsertAt('.',3);
-      self.time--;
-      let timeStr = String(self.time).padStart(5, '0');
-      self.timerElement.innerHTML = "Time : " + timeStr.slice(0, 3) + '.' + timeStr.slice(3);
-      if (self.time == 0) {
-        expireFunction();
-        clearInterval(self.intervalControler);
+      if (self.timeRunning) {
+        let timeStr = String(self.time).padStart(5, '0');
+        self.timerElement.innerHTML = "Time : " + timeStr.slice(0, 3) + '.' + timeStr.slice(3);
+        if (self.time == 0) {
+          expireFunction();
+          clearInterval(self.intervalControler);
+          self.timeRunning = false;
+        }
+        self.time--;
       }
     }, 10);
   }
