a9258dd209cc4e6baeb1717e8b4f6b246b34fee8
Axel O <olougounaaxel@gmail.com>
2023-01-05 10:32:29 +0100

    modif d'insertion

--- modules/scoreboard.mjs
+++ modules/scoreboard.mjs
@@ -2,48 +2,67 @@ export class Gamer {
     constructor(Name, Score){
         this.name = Name;
         this.score = Score;
+        this.gamers = []
     }
     
-}
+    Scoreboard()
+    {
+        let alias = prompt("Entre ton nom", "User");
+        //let gamers = [];
+        //self.gamers
+        this.gamers.push( new Gamer(alias, 0));
+        //console.log(gamers);
+        //let game = new Gamer ("test", 0);
+        //let table = document.getElementById("scoreTable");
 
-export function Scoreboard()
-{
-    let alias = prompt("Entre ton nom", "User");
-    let gamers = [];
-    gamers.push( new Gamer(alias, 0));
-    console.log(gamers);
-    //let table = document.getElementById("scoreTable");
+        //console.log(table);
+        //console.log(tableau);
+        this.renderArray();
 
-    //console.log(table);
-    //console.log(tableau);
-    renderArray(gamers);
+    }
 
-}
+    addGamer()
+    {
+        
+    }
+    removeGamer(ev)
+    {
+        this.gamers.splice(ev.target.getAttribute("array-index"), 1);
+        renderArray();
+    }
 
-export function renderArray(gamers)
-{
-    let tableau= document.getElementById("scoreTable");
-    console.log(tableau);
-    tableau.innerText = "";
-    gamers.forEach((gamer, index) => {
-            let tableRow = document.createElement("tr");
-            let nameTd = document.createElement("td");
-            let scoreTd = document.createElement("td");
-            //rmvTd = document.createElement("td");
-            //rmvButton = document.createElement("button");
-            //rmvButton.innerText = "delete";
-            //rmvButton.setAttribute("array-index", index);
-            //rmvButton.addEventListener("click", removeStudent);
-            //rmvTd.appendChild(rmvButton);
+    renderArray()
+    {
+        let tableau= document.getElementById("scoreTable");
+        //console.log(tableau);
+        tableau.innerText = "";
+        this.gamers.forEach((gamer, index) => {
+                let tableRow = document.createElement("tr");
+                let nameTd = document.createElement("td");
+                let scoreTd = document.createElement("td");
+                let rmvTd = document.createElement("td");
+                let rmvButton = document.createElement("button");
+                rmvButton.innerText = "delete";
+                rmvButton.setAttribute("array-index", index);
+                rmvButton.addEventListener("click", gamer.removeGamer);
+                rmvTd.appendChild(rmvButton);
 
-            nameTd.innerText = gamer.name;
-            scoreTd.innerText = gamer.score;
+                nameTd.innerText = gamer.name;
+                scoreTd.innerText = gamer.score;
 
-            tableRow.appendChild(nameTd);
-            tableRow.appendChild(scoreTd);
-            //tableRow.appendChild(rmvTd);
-            tableau.appendChild(tableRow);
-    
-    });
-    console.log(tableau);
+                tableRow.appendChild(nameTd);
+                tableRow.appendChild(scoreTd);
+                tableRow.appendChild(rmvTd);
+                tableau.appendChild(tableRow);
+        
+        });
+        //console.log(tableau);
+    }
+
+
+    UpdateScore()
+    {
+        
+    }
+
 }
